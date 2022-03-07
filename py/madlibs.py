3c7ba104495b69ff9450c226308a68a181d2c103
Oluwasegun Atolagbe <oluwasegun.atolagbe@mail.citytech.cuny.edu>
2022-03-07 17:05:04 -0500

    Add madlibs part 1 PY file

---
+++ py/madlibs.py
@@ -0,0 +1,9 @@
+noun = "den";
+verb = "burning";
+adjective = "beautiful";
+adverb = "Finding";
+        
+print("On a tropical " + noun + ",");
+print("Underneath a " + verb + " moon.");
+print("Hangin\' with the " + adjective + " dancers,");
+print(adverb + " questions cause\' they got all he answers.");
